fd54482ba20df0e8fd56650ca07286ad927aba81
sukrit007 <sukrit007@gmail.com>
2014-09-05 12:48:55 -0700

    Add check for location name

--- yoda/model.py
+++ yoda/model.py
@@ -1,13 +1,17 @@
 __author__ = 'sukrit'
 
+import re
+
+INVALID_LOCATION_CHARS = re.compile('[^A-Za-z\\-]+')
 
 class Location:
-    def __init__(self, upstream, path='/', location_name='home',
+    def __init__(self, upstream, path='/', location_name=None,
                  allowed_acls=['public'], denied_acls=['global-black-list'],
                  min_nodes=1):
         self.upstream = upstream
         self.path = path
-        self.location_name = location_name
+        self.location_name = INVALID_LOCATION_CHARS.sub(
+            '-', location_name or path)
         self.allowed_acls = allowed_acls
         self.denied_acls = denied_acls
     pass
