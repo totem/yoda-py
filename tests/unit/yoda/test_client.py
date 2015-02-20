4e5db59200034cf77bfc66dda8ad3d47fc6cade8
sukrit007 <sukrit007@gmail.com>
2015-02-20 16:21:38 -0700

    Add ability to  renew proxy nodes

--- tests/unit/yoda/test_client.py
+++ tests/unit/yoda/test_client.py
@@ -5,7 +5,7 @@ import etcd
 from mock import MagicMock
 from nose.tools import eq_
 
-from yoda.client import as_upstream, Client
+from yoda.client import as_upstream, Client, as_endpoint
 
 MOCK_APP_NAME = 'mock-app'
 MOCK_APP_VERSION = 'mock-version'
@@ -30,6 +30,33 @@ def test_as_upstream_without_version():
 
     # Then: Expected upstream is created
     eq_(upstream, 'mock-app-80')
+
+
+def test_as_endpoint():
+    """
+    Should return endpoint the endpoint value for a given host, port
+    """
+
+    # When: I invoke as_endpoint with existing host and port
+    endpoint = as_endpoint('mockhost', 40123)
+
+    # Then: Expected upstream is created
+    eq_(endpoint, 'mockhost:40123')
+
+
+def test_client_init():
+    """
+    Should initialize etcd_client when etcd client instance is not passed
+    explicitly
+    """
+
+    # When: I create new instance of yoda client
+    client = Client()
+
+    # Then: Client gets initialized as expected
+    eq_(client.etcd_base, '/yoda')
+    eq_(client.etcd_cl.host, 'localhost')
+    eq_(client.etcd_cl.port, 4001)
 
 
 class TestClient():
@@ -59,9 +86,10 @@ class TestClient():
         self.client.register_upstream('test')
 
         # Then: The upstream gets created successfully.
-        self.etcd_cl.set.call_count = 1
-        self.etcd_cl.set.assert_called_with('/yoda/upstreams/test/mode',
-                                            'http')
+        self.etcd_cl.set.assert_called_once_with(
+            '/yoda/upstreams/test/mode', 'http')
+        self.etcd_cl.write.assert_called_with(
+            '/yoda/upstreams/test', dir=True, ttl=3600)
 
     def test_register_upstream_with_uri_timeout_interval(self):
         """
@@ -74,7 +102,6 @@ class TestClient():
                                       health_interval='5m')
 
         # Then: The upstream gets created successfully.
-        self.etcd_cl.set.call_count = 3
         self.etcd_cl.set.assert_any_call('/yoda/upstreams/test/mode', 'http')
         self.etcd_cl.set.assert_any_call('/yoda/upstreams/test/health/uri',
                                          '/')
@@ -82,6 +109,8 @@ class TestClient():
                                          '5s')
         self.etcd_cl.set.assert_any_call(
             '/yoda/upstreams/test/health/interval', '5m')
+        self.etcd_cl.write.assert_called_with(
+            '/yoda/upstreams/test', dir=True, ttl=3600)
 
     def test_discover_node(self):
         """
@@ -92,8 +121,7 @@ class TestClient():
         self.client.discover_node('test', 'testnode', 'localhost:3434')
 
         # Then: My node gets registered successfully.
-        self.etcd_cl.set.call_count = 1
-        self.etcd_cl.set.assert_called_with(
+        self.etcd_cl.set.assert_called_once_with(
             '/yoda/upstreams/test/endpoints/testnode', 'localhost:3434',
             ttl=120)
 
@@ -105,6 +133,26 @@ class TestClient():
         self.client.discover_proxy_node('test', 'localhost')
 
         # Then: My proxy node gets registered successfully.
-        self.etcd_cl.set.call_count = 1
-        self.etcd_cl.set.assert_called_with('/yoda/proxy-nodes/test',
-                                            'localhost', ttl=300)
+        self.etcd_cl.set.assert_called_once_with(
+            '/yoda/proxy-nodes/test', 'localhost', ttl=300)
+
+    def test_renew_upstream(self):
+        """
+        Should renew a given upstream
+        """
+
+        # When: I renew existing upstream
+        self.client.renew_upstream('mock')
+
+        # Then: My upstream gets registered as expected
+        self.etcd_cl.write.assert_called_with(
+            '/yoda/upstreams/mock', dir=True, ttl=3600, prevExist=True)
+
+    def test_etcd_safe_delete_for_non_existing_key(self):
+        # Given: Non existing key
+        self.etcd_cl.delete.side_effect = KeyError('mock')
+
+        # When: I perform a safe delete for mock key
+        self.client._etcd_safe_delete('mock')
+
+        # Then: No exception is thrown
